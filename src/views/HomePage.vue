935fec20e98a665c294b237273f469c2032c9fb4
Ange <9709929+angecode@user.noreply.gitee.com>
2022-11-27 03:15:23 +0800

    还没完

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,8 +11,10 @@
           @select="handleSelect"
         >
           <div class="flex-grow" />
+          <!-- LOGO -->
           <el-menu-item index="0">qiliqili</el-menu-item>
           <div class="flex-grow" />
+          <!-- 搜索框 -->
           <el-input
             v-model="headmenu.searchValue"
             placeholder="请输入关键词"
@@ -23,6 +25,7 @@
             </template>
           </el-input>
           <div class="flex-grow" />
+          <!-- 头像 -->
           <el-menu-item
             index="1"
             @mouseover="avatarOver"
@@ -30,14 +33,35 @@
           >
             <el-avatar :size="40" :src="circleUrl" :style="avatarUse" />
           </el-menu-item>
-          <el-menu-item index="2">消息</el-menu-item>
-          <el-menu-item index="3">动态</el-menu-item>
+          <!-- 消息的下拉菜单 -->
+          <el-dropdown>
+            <el-menu-item index="2">
+              <span class="el-dropdown-link"> 消息 </span>
+            </el-menu-item>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>回复我的</el-dropdown-item>
+                <el-dropdown-item>@我的</el-dropdown-item>
+                <el-dropdown-item>收到的赞</el-dropdown-item>
+                <el-dropdown-item>系统通知</el-dropdown-item>
+                <el-dropdown-item>我的消息</el-dropdown-item>
+                <el-dropdown-item>私信存档</el-dropdown-item>
+                <el-dropdown-item>消息设置</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          <el-menu-item
+            index="3"
+            @mouseover="dynamicFun"
+            @mouseleave="dynamicState = false"
+            >动态</el-menu-item
+          >
           <el-menu-item index="4">订阅</el-menu-item>
           <el-menu-item index="5">收藏</el-menu-item>
           <el-menu-item index="6">历史</el-menu-item>
           <div class="flex-grow" />
         </el-menu>
-
+        <!-- 头像悬浮信息 -->
         <el-card
           v-show="avatarState"
           class="avatarInfo"
@@ -153,6 +177,99 @@
             >退出登录</el-button
           >
         </el-card>
+        <!-- 动态悬浮信息 -->
+        <div class="dynamic" v-show="(dynamicState = true)">
+          <!-- 动态菜单 -->
+          <div
+            style="
+              display: flex;
+              flex-direction: row;
+              flex-wrap: nowrap;
+              align-content: center;
+              justify-content: space-around;
+              align-items: center;
+            "
+          >
+            <div style="width: 100px; display: flex; flex-wrap: nowrap">
+              <el-button text size="small" @click="dyVideo" :style="dyVideoCss"
+                >视频</el-button
+              >
+              <el-button text size="small" @click="dyAnime" :style="dyAnimeCss"
+                >番剧</el-button
+              >
+              <el-button text size="small" @click="dyCol" :style="dyColCss"
+                >专栏</el-button
+              >
+              <el-button text size="small" @click="dyLive" :style="dyLiveCss"
+                >直播</el-button
+              >
+            </div>
+
+            <el-button round style="margin-left: 87px">全部动态</el-button>
+          </div>
+          <!-- 动态内容 -->
+          <!-- 视频列表 -->
+          <div v-if="dynamicType == 0">
+            <div class="dyvideo">
+              <div v-for="i in 10" :key="i" style="margin-bottom: 10px">
+                <el-card
+                  class="box-card"
+                  style="width: 145px; height: 150px; border-radius: 15px"
+                  body-style="padding:0px"
+                >
+                  <div
+                    style="background-color: #909399; width: 100%; height: 85px"
+                  >
+                    封面{{ i }}
+                  </div>
+                  <h5 style="margin: 5px 0px 3px 11px">视频{{ i }}的介绍</h5>
+                  <el-button round size="small" style="margin: 13px 0px 0px 5px"
+                    >视频{{ i }}的作者</el-button
+                  >
+                </el-card>
+              </div>
+            </div>
+          </div>
+          <!-- 番剧列表 -->
+          <div v-if="dynamicType == 1">
+            <div class="dyanime">
+              <div
+                v-for="i in 10"
+                :key="i"
+                style="
+                  margin-bottom: 10px;
+                  width: 98%;
+                  border: 1px solid #e5eaf3;
+                  border-radius: 15px;
+                  height: 70px;
+                  display: flex;
+                "
+              >
+                <div
+                  style="
+                    background-color: #909399;
+                    width: 40%;
+                    height: 100%;
+                    border-radius: 15px;
+                  "
+                >
+                  封面{{ i }}
+                </div>
+                <div
+                  style="
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    margin-left: 5px;
+                  "
+                >
+                  <h4 style="margin-bottom: -15px;">集名{{ i }}</h4>
+                  <h5>番名{{ i }}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </el-header>
       <!-- 主体 -->
       <el-main>Main</el-main>
@@ -169,11 +286,11 @@ export default {
   name: "HomePage",
 
   setup() {
-    // 数据
+    // 菜单栏
     let headmenu = reactive({
       searchValue: "",
     });
-
+    // 头像
     let avatar = reactive({
       avatarState: false,
       percentage: 0,
@@ -213,17 +330,60 @@ export default {
         avatar.avatarUse = avatar.avatarOri;
       },
     });
+    // 动态
+    let dynamic = reactive({
+      dynamicState: false,
+      dynamicType: 0,
+      dyVideoCss: "width: 0px;font-size:15px",
+      dyAnimeCss: "width: 0px;font-size:15px",
+      dyColCss: "width: 0px;font-size:15px",
+      dyLiveCss: "width: 0px;font-size:15px",
+      borderColor: "width: 0px;color: #409EFF;font-size:15px",
+      dynamicFun() {
+        this.dyVideo();
+        dynamic.dynamicState = true;
+      },
+      dyVideo() {
+        dynamic.dynamicType = 0;
+        dynamic.dyVideoCss = dynamic.borderColor;
+        dynamic.dyAnimeCss = "";
+        dynamic.dyColCss = "";
+        dynamic.dyLiveCss = "";
+      },
+      dyAnime() {
+        dynamic.dynamicType = 1;
+        dynamic.dyAnimeCss = dynamic.borderColor;
+        dynamic.dyVideoCss = "";
+        dynamic.dyColCss = "";
+        dynamic.dyLiveCss = "";
+      },
+      dyCol() {
+        dynamic.dynamicType = 2;
+        dynamic.dyAnimeCss = "";
+        dynamic.dyVideoCss = "";
+        dynamic.dyColCss = dynamic.borderColor;
+        dynamic.dyLiveCss = "";
+      },
+      dyLive() {
+        dynamic.dynamicType = 3;
+        dynamic.dyAnimeCss = "";
+        dynamic.dyVideoCss = "";
+        dynamic.dyColCss = "";
+        dynamic.dyLiveCss = dynamic.borderColor;
+      },
+    });
 
     let refAvatar = toRefs(avatar);
+    let refDynamic = toRefs(dynamic);
     // 生命函数
     onMounted(() => {
       getPercentage();
       console.log("获取经验值和等级成功");
     });
 
-    // 方法
+    // 获取经验值显示等级和进度条
     function getPercentage() {
-      let xp=avatar.xp
+      let xp = avatar.xp;
       let percentage = Math.round((xp / 10000) * 100);
       let level = "";
       let levelCss = "";
@@ -249,10 +409,14 @@ export default {
         level = "Max";
         levelCss = "font-size:13px;color:#E6A23C;font-weight: bold;";
       }
-      return (avatar.percentage = percentage), (avatar.level = level), (avatar.levelCss = levelCss);
+      return (
+        (avatar.percentage = percentage),
+        (avatar.level = level),
+        (avatar.levelCss = levelCss)
+      );
     }
 
-    return { headmenu, ...refAvatar };
+    return { headmenu, ...refAvatar, ...refDynamic };
   },
 };
 </script>
@@ -268,11 +432,58 @@ export default {
   height: 45px;
   margin-top: 3px;
 }
+
 .avatarInfo {
   z-index: 998;
   width: 200px;
   height: 380px;
   position: absolute;
   left: 1170px;
+  border-radius: 20px;
+}
+
+.dynamic {
+  width: 340px;
+  height: 520px;
+  z-index: 998;
+  position: absolute;
+  left: 1240px;
+  border-radius: 20px;
+  border: 1px solid #e5eaf3;
+  padding: 5px 10px;
+}
+
+.dyvideo {
+  margin-top: 10px;
+  height: 483px;
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+.dyvideo::-webkit-scrollbar {
+  display: none;
+}
+.dyanime {
+  overflow-y: auto;
+  display: flex;
+  // flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 10px;
+  height: 483px;
+}
+.dyanime::-webkit-scrollbar {
+  display: none;
+}
+
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+  font-size: 15px;
 }
 </style>
