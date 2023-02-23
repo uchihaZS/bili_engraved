7c862a8334a1a59fb14a1056fe0f9a60d6cfc2f6
Ange <9709929+angecode@user.noreply.gitee.com>
2023-02-23 22:59:25 +0800

    动态页完成

--- src/views/Dynamic.vue
+++ src/views/Dynamic.vue
@@ -70,7 +70,15 @@
                 </div>
               </el-aside>
               <!-- 中间 -->
-              <el-main>
+              <el-main
+                style="
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  justify-content: flex-start;
+                "
+              >
+                <!-- 发表动态 -->
                 <div
                   style="
                     width: 1050px;
@@ -78,10 +86,9 @@
                     background-color: white;
                     margin-top: 0px;
                     border-radius: 5px;
-                    
                   "
                 >
-                <!-- 输入框 -->
+                  <!-- 输入框 -->
                   <el-input
                     v-model="textarea"
                     :rows="4"
@@ -98,8 +105,15 @@
                       resize: none;
                     "
                   />
-                  <div style="display:flex">
-                    <div style="border-right:1px solid #CFD3DC;width:91%;display:flex;padding:0px 10px" >
+                  <div style="display: flex">
+                    <div
+                      style="
+                        border-right: 1px solid #cfd3dc;
+                        width: 91%;
+                        display: flex;
+                        padding: 0px 10px;
+                      "
+                    >
                       <el-button type="info" plain>表情</el-button>
                       <el-button type="info" plain>图片</el-button>
                       <el-button type="info" plain>艾特</el-button>
@@ -107,7 +121,266 @@
                       <el-button type="info" plain>定时</el-button>
                       <el-button type="info" plain>直播</el-button>
                     </div>
-                    <el-button type="primary" style="margin-left:10px">发送</el-button>
+                    <el-button type="primary" style="margin-left: 10px"
+                      >发送</el-button
+                    >
+                  </div>
+                </div>
+                <!-- 横向列表 -->
+                <div
+                  style="
+                    width: 1050px;
+                    height: 100px;
+                    background-color: white;
+                    margin-top: 20px;
+                    border-radius: 5px;
+                  "
+                >
+                  <el-scrollbar>
+                    <div style="display: flex">
+                      <div
+                        v-for="i in 20"
+                        :key="i"
+                        style="
+                          width: 80px;
+                          flex-shrink: 0;
+                          display: flex;
+                          flex-direction: column;
+                          align-items: center;
+                          margin-top: 10px;
+                        "
+                      >
+                        <el-avatar :size="40" :src="circleUrl"></el-avatar>
+                        <p style="margin-top: 10px">用户名{{ i }}</p>
+                      </div>
+                    </div>
+                  </el-scrollbar>
+                </div>
+                <!-- 菜单栏 -->
+                <div
+                  style="
+                    width: 1050px;
+                    height: 50px;
+                    background-color: white;
+                    margin-top: 20px;
+                    border-radius: 5px;
+                  "
+                >
+                  <div style="display: flex; justify-content: space-evenly">
+                    <div class="tag">
+                      <div>全部</div>
+                      <div class="direction"></div>
+                    </div>
+                    <div class="tag">
+                      <div>视频投稿</div>
+                      <div class="direction"></div>
+                    </div>
+                    <div class="tag">
+                      <div>追番追剧</div>
+                      <div class="direction"></div>
+                    </div>
+                    <div class="tag">
+                      <div>专栏</div>
+                      <div class="direction"></div>
+                    </div>
+                  </div>
+                </div>
+                <!-- 动态列表 -->
+                <div style="width: 1050px; height: auto">
+                  <!-- 视频动态 -->
+                  <div
+                    style="
+                      width: 100%;
+                      height: auto;
+                      border-radius: 10px;
+                      margin-top: 10px;
+                      background-color: white;
+                      box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+                    "
+                  >
+                    <!-- 头像栏 -->
+                    <div
+                      style="
+                        width: 100%;
+                        padding: 0px 15px;
+                        height: 60px;
+                        display: flex;
+                        align-items: center;
+                        position: relative;
+                        top: 50px;
+                        left: 31px;
+                      "
+                    >
+                      <el-avatar :size="60" :src="circleUrl"></el-avatar>
+                      <div style="margin-left: 15px; height: 60px">
+                        <p style="font-size: 18px; margin: 0px auto">用户名</p>
+                        <div style="margin-top: 10px; font-size: 14px">
+                          <span>发布时间</span>
+                          <span style="margin-left: 10px">投稿了视频</span>
+                        </div>
+                      </div>
+                      <el-dropdown
+                        trigger="click"
+                        style="margin-left: 765px; margin-top: 0px"
+                      >
+                        <el-icon
+                          style="
+                            transform: rotate(90deg);
+                            width: 25px;
+                            height: 25px;
+                          "
+                          size="large"
+                          ><MoreFilled style="width: 25px; height: 25px"
+                        /></el-icon>
+                        <template #dropdown>
+                          <el-dropdown-menu>
+                            <el-dropdown-item>举报</el-dropdown-item>
+                          </el-dropdown-menu>
+                        </template>
+                      </el-dropdown>
+                    </div>
+                    <!-- 视频框 -->
+                    <div
+                      style="
+                        margin-top: 70px;
+                        display: flex;
+                        margin-left: 120px;
+                        border-radius: 5px;
+                        border: 1px solid gray;
+                        width: 500px;
+                      "
+                    >
+                      <div
+                        style="
+                          width: 200px;
+                          height: 140px;
+                          background-color: #909399;
+                        "
+                      ></div>
+                      <div
+                        style="
+                          display: flex;
+                          flex-direction: column;
+                          width: 200px;
+                          margin-left: 15px;
+                        "
+                      >
+                        <p
+                          style="font-size: 18px; margin: 0px 0px"
+                          class="toBlue"
+                        >
+                          视频名
+                        </p>
+                        <div style="margin-top: 80px">
+                          <span style="font-size: 14px">播放数</span>
+                          <span style="margin-left: 20px; font-size: 14px">
+                            弹幕数
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- 底栏 -->
+                    <div
+                      style="
+                        display: flex;
+                        width: 400px;
+                        margin: 10px 0px 10px 120px;
+                        flex-direction: row;
+                        height: 50px;
+                        justify-content: flex-start;
+                      "
+                    >
+                      <span>分享</span>
+                      <span style="margin-left: 40px">评论</span>
+                      <span style="margin-left: 40px">点赞</span>
+                    </div>
+                  </div>
+                  <!-- 普通动态 -->
+                  <div
+                    style="
+                      width: 100%;
+                      height: auto;
+                      border-radius: 10px;
+                      margin-top: 10px;
+                      background-color: white;
+                      box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+                    "
+                  >
+                    <!-- 头像栏 -->
+                    <div
+                      style="
+                        width: 100%;
+                        padding: 0px 15px;
+                        height: 60px;
+                        display: flex;
+                        align-items:center;
+                        position: relative;
+                        top: 50px;
+                        left: 31px;
+                      "
+                    >
+                      <el-avatar :size="60" :src="circleUrl"></el-avatar>
+                      <div style="margin-left: 15px; height: 60px">
+                        <p style="font-size: 18px; margin: 0px auto">用户名</p>
+                        <div style="margin-top: 10px; font-size: 14px">
+                          <span>发布时间</span>
+                          <span style="margin-left: 10px">投稿了视频</span>
+                        </div>
+                      </div>
+                      <el-dropdown
+                        trigger="click"
+                        style="margin-left: 765px; margin-top: 0px"
+                      >
+                        <el-icon
+                          style="
+                            transform: rotate(90deg);
+                            width: 25px;
+                            height: 25px;
+                          "
+                          size="large"
+                          ><MoreFilled style="width: 25px; height: 25px"
+                        /></el-icon>
+                        <template #dropdown>
+                          <el-dropdown-menu>
+                            <el-dropdown-item>举报</el-dropdown-item>
+                          </el-dropdown-menu>
+                        </template>
+                      </el-dropdown>
+                    </div>
+                    <!-- 内容框 -->
+                    <div
+                      style="
+                        width: 500px;
+                        height: auto;
+                        margin-top: 51px;
+                        margin-left: 120px;
+                      "
+                    >
+                      <p>
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                      </p>
+                      <p>
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                      </p>
+                      <p>
+                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                      </p>
+                    </div>
+                    <!-- 底栏 -->
+                    <div
+                      style="
+                        display: flex;
+                        width: 400px;
+                        margin: 10px 0px 10px 120px;
+                        flex-direction: row;
+                        height: 50px;
+                        justify-content: flex-start;
+                      "
+                    >
+                      <span>分享</span>
+                      <span style="margin-left: 40px">评论</span>
+                      <span style="margin-left: 40px">点赞</span>
+                    </div>
                   </div>
                 </div>
               </el-main>
@@ -160,13 +433,15 @@
 <script>
 import HeaderNav from "@/components/HeaderNav.vue";
 import { reactive, ref, toRefs } from "vue";
+import { MoreFilled } from "@element-plus/icons-vue";
 export default {
-  components: { HeaderNav },
+  components: { HeaderNav,MoreFilled },
   setup() {
     const circleUrl =
       "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png";
 
     let textarea = ref("");
+
     return {
       circleUrl,
       textarea,
@@ -218,8 +493,27 @@ export default {
 .avatag:hover {
   color: #409eff;
 }
-
-.el-input {
-  border: 0;
+.tag {
+  font-size: 18px;
+  height: 75px;
+  margin-top: 12px;
+  align-items: center;
+  display: flex;
+  width: auto;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  // justify-content: center;
+}
+.tag:hover {
+  color: #409eff;
+}
+.tag:hover .direction {
+  background-color: #409eff;
+}
+.direction {
+  height: 2px;
+  width: 100%;
+  position: relative;
+  top: 13px;
 }
 </style>
